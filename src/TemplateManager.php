8f225becefaa04558f22a6edd6086670df56cb17
John Saulnier <jpsymfony@free.fr>
2019-06-10 09:50:31 +0200

    added hydrateMethods into builder before refactoring

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -23,4 +23,56 @@ class TemplateManager extends TemplateBuilder
     {
         return (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
     }
+
+    /**
+     * @inheritDoc
+     */
+    protected function hydrateTextWithQuote(string &$text, ?Quote $quote = null): void
+    {
+        if ($quote)
+        {
+            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
+            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
+            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
+
+            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
+            $containsSummary     = strpos($text, '[quote:summary]');
+
+            if ($containsSummaryHtml !== false || $containsSummary !== false) {
+                if ($containsSummaryHtml !== false) {
+                    $text = str_replace(
+                        '[quote:summary_html]',
+                        Quote::renderHtml($_quoteFromRepository),
+                        $text
+                    );
+                }
+                if ($containsSummary !== false) {
+                    $text = str_replace(
+                        '[quote:summary]',
+                        Quote::renderText($_quoteFromRepository),
+                        $text
+                    );
+                }
+            }
+
+            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+        }
+
+        if (strpos($text, '[quote:destination_link]') !== false) {
+            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destinationOfQuote->countryName . '/quote/' . $_quoteFromRepository->id, $text);
+        } else {
+            $text = str_replace('[quote:destination_link]', '', $text);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function hydrateTextWithUser(string &$text, array $data): void
+    {
+        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $this->applicationContext->getCurrentUser();
+        if($_user) {
+            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
+        }
+    }
 }
